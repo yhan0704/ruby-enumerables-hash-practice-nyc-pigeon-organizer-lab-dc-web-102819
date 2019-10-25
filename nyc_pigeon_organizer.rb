96b687c5c10e31de9d55588e4c1640bc5141688a
Young Han <han20@umbc.edu>
2019-10-25 00:54:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+ new_hash={}
+   pigeon_data.each do |key, value|
+    value.each do |second_key, second_value|
+      second_value.each do |third_key|
+        if !new_hash[third_key]
+          new_hash[third_key] = {}
+        end
+
+        if !new_hash[third_key][key]
+          new_hash[third_key][key] = []
+        end
+
+        new_hash[third_key][key] << second_key.to_s
+
+      end
+    end
+  end
+  new_hash
 end
