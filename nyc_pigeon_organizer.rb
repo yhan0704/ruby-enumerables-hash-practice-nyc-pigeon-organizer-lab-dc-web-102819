67e1703125128c028df459559cc983c328dfed06
Young Han <han20@umbc.edu>
2019-10-25 00:39:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,11 @@
 def nyc_pigeon_organizer(data)
   new_hash={}
-   data.each do |key, value|
-    value.each do |
+   data.each do |first_key, first_value|
+    value.each do |second_key, second_value|
+     pp second_value do |third_key, third_value|
+      
+      end
+    end
   end
   new_hash
 end
